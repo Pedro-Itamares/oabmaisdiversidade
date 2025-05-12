c724cc98edf784c326af4cecf229df11964b986d
Pedro-Itamares <pedroitamares.smas@gmail.com>
2025-05-12 12:17:57 -0300

    teste

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-container a");
+    const sections = document.querySelectorAll("section");
+
+    window.addEventListener("scroll", function() {
+        let current = "";
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            if (pageYOffset >= sectionTop - 60) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").includes(current)) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
+*/
